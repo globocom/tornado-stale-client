928cfb26f7466855a992daa0303475e71ed511e9
Enrico <rico.bl@gmail.com>
2016-02-19 10:51:02 -0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tornado-stale-client',
-    version='0.0.1',
+    version='0.0.2',
     description='An async http client for tornado with stale cache support',
     long_description='',
     keywords='tornado async http client redis stale cache',
